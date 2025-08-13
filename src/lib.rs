89608eb428a00d6fa825d17abe355b284bf285b5
Patrick Amrein <amrein@ubique.ch>
2025-08-13 11:01:57 +0200

    expose josekit jwk

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,10 @@ software distributed under the License is distributed on an
 "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
-under the License.   
+under the License.
  */
 pub mod jwt;
 pub mod models;
+
+pub use josekit::jwk::Jwk;
+pub use josekit::jwk::JwkSet;
